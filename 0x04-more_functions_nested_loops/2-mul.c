bd39b1daffbba3761caaa61495cf854eddaf326f
Kamva-63 <lonwabo.kamva.8@gmail.com>
2023-03-24 12:42:25 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,12 +2,12 @@
 
 /**
  * mul - Multiplies two intergers
- * @x: The first number to multiply
- * @y: The second number to multiply with first number
- * Return: The result of multiplication between x param and y param
+ * @a: The first number to multiply
+ * @b: The second number to multiply with first number
+ * Return: The result of multiplication between a param and b param
  */
 
-int null(int x, int y)
+int null(int a, int b)
 {
-	return (x * y);
+	return (a * b);
 }
