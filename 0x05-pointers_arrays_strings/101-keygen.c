0def5c8b9b8ac0fb35030ee3c42bc759ba427f7d
Kamva-63 <lonwabo.kamva.8@gmail.com>
2023-03-27 15:27:52 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,6 +6,7 @@
  * @src: Source value
  * Return: The pointer to dest
  */
+
 char *_strcpy(char *dest, char *src)
 {
 	int i;
@@ -13,6 +14,6 @@ char *_strcpy(char *dest, char *src)
 	{
 		dest[i] = src[i];
 	}
-	dest[i++] = '\0';
+	dest[i++] = '\0'; 
 	return (dest);
 }
